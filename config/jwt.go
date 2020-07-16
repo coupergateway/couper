2502b307420385f599c7cf43aaf892cc7c7d5342
Johannes Koch <johannes.koch@avenga.com>
2020-07-16 18:29:18 +0200

    JWT-Validierung

--- config/jwt.go
+++ config/jwt.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"strings"
+	"crypto/rsa"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/sirupsen/logrus"
 )
 
@@ -22,12 +26,59 @@ type Jwt struct {
 	SignatureAlgorithm string  `hcl:"signature_algorithm"`
 	Claims             *Claims `hcl:"claims,block"`
 	log                *logrus.Entry
+	parser             *jwt.Parser
+	key                *rsa.PublicKey
 }
+
+// TODO read values from Env
 
 func (j *Jwt) Init(log *logrus.Entry) {
 	j.log = log
+	if j.KeyFile != "" {
+		pem, err := ioutil.ReadFile(j.KeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		j.key, err = jwt.ParseRSAPublicKeyFromPEM([]byte(pem))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// TODO j.Key
+	var options []jwt.ParserOption
+	options = append(options, jwt.WithValidMethods([]string{j.SignatureAlgorithm}))
+	if j.Claims.Issuer != "" {
+		options = append(options, jwt.WithIssuer(j.Claims.Issuer))
+	}
+	if j.Claims.Audience != "" {
+		options = append(options, jwt.WithAudience(j.Claims.Audience))
+	} else {
+		options = append(options, jwt.WithoutAudienceValidation())
+	}
+	j.parser = jwt.NewParser(options...)
 }
 
 func (j *Jwt) Check(req *http.Request) bool {
+	tokenValue := ""
+	if j.Header != "" {
+		tokenValue = req.Header.Get(j.Header)
+		if j.Header == "Authorization" {
+			// TODO handle Bearer case-insensitively
+			tokenValue = strings.TrimPrefix(tokenValue, "Bearer ")
+		}
+	}
+	// TODO j.Cookie, j.PostParam, j.QueryParam
+	if tokenValue == "" {
+		j.log.Error("token is empty")
+		return false
+	}
+	token, err := j.parser.Parse(tokenValue, func(token *jwt.Token) (interface{}, error) {
+		return j.key, nil
+	})
+	if err != nil {
+		j.log.Error(err)
+		return false
+	}
+	j.log.WithField("token", token).Debug()
 	return true
 }
