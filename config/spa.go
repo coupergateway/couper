e692db9ad56d48b97cc6b378dc401101c0d4e489
Marcel Ludwig <marcel.ludwig@avenga.com>
2020-07-21 17:06:47 +0200

    spa ac

--- config/spa.go
+++ config/spa.go
@@ -1,6 +1,7 @@
 package config
 
 type Spa struct {
+	AccessControl []string `hcl:"access_control,optional"`
 	BasePath      string   `hcl:"base_path,optional"`
 	BootstrapFile string   `hcl:"bootstrap_file"`
 	Paths         []string `hcl:"paths"`
