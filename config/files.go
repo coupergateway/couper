138943921b24f67a8963d5b82eff1bb9aecb0000
Alex Schneider <alex.schneider@sevenval.com>
2020-07-22 11:10:22 +0200

    Add optional @base_path@ for Files, see #47299-files-serving

--- config/files.go
+++ config/files.go
@@ -1,6 +1,7 @@
 package config
 
 type Files struct {
+	BasePath     string `hcl:"base_path,optional"`
 	DocumentRoot string `hcl:"document_root"`
 	ErrorFile    string `hcl:"error_file,optional"` // TODO: error_${status}.html ?
 }
