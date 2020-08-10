4a8b8cf98a4360ad6d9f74d2ad954bd4eca7e71d
Marcel Ludwig <marcel.ludwig@avenga.com>
2020-08-10 12:57:51 +0200

    add spa tests
    do not serve directories or any other file

---
+++ handler/spa_test.go
@@ -0,0 +1,43 @@
+package handler_test
+
+import (
+	"go.avenga.cloud/couper/gateway/handler"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestSpa_ServeHTTP(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	tests := []struct {
+		name   string
+		filePath string
+		req   *http.Request
+		expectedCode int
+	}{
+		{"serve bootstrap file", "testdata/spa/app.html", httptest.NewRequest(http.MethodGet, "/", nil), http.StatusOK},
+		{"serve no bootstrap file", "testdata/spa/not_exist.html", httptest.NewRequest(http.MethodGet, "/", nil), http.StatusNotFound},
+		{"serve bootstrap dir", "testdata/spa", httptest.NewRequest(http.MethodGet, "/", nil), http.StatusInternalServerError},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := handler.NewSpa(wd, tt.filePath)
+
+			res := httptest.NewRecorder()
+			s.ServeHTTP(res, tt.req)
+
+			if !res.Flushed {
+				res.Flush()
+			}
+
+			if res.Code != tt.expectedCode {
+				t.Errorf("Expected status code %d, got: %d", tt.expectedCode, res.Code)
+			}
+		})
+	}
+}
