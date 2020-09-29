a96faf0173195d54697ef44358bdb8725cb52806
Marcel Ludwig <marcel.ludwig@avenga.com>
2020-09-29 09:33:26 +0200

    Remove unused req.id option

--- config/settings.go
+++ config/settings.go
@@ -1,9 +1,8 @@
 package config
 
 type Settings struct {
-	DefaultPort     int    `hcl:"default_port,optional"`
-	XForwardedHost  bool   `hcl:"xfh,optional"`
-	HealthPath      string `hcl:"health_path,optional"`
-	RequestIDFormat string `hcl:"request_id_format,optional"`
-	LogFormat       string `hcl:"log_format,optional"`
+	DefaultPort    int    `hcl:"default_port,optional"`
+	XForwardedHost bool   `hcl:"xfh,optional"`
+	HealthPath     string `hcl:"health_path,optional"`
+	LogFormat      string `hcl:"log_format,optional"`
 }
