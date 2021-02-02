bdee80940725c2d24f46fb6dfb4ce9a2670ded14
Alex Schneider <alex.schneider@sevenval.com>
2021-02-02 17:43:23 +0100

    Add `disable` flag to CORS

--- config/cors.go
+++ config/cors.go
@@ -7,5 +7,6 @@ import (
 type CORS struct {
 	AllowedOrigins   cty.Value `hcl:"allowed_origins"`
 	AllowCredentials bool      `hcl:"allow_credentials,optional"`
+	Disable          bool      `hcl:"disable,optional"`
 	MaxAge           string    `hcl:"max_age,optional"`
 }
