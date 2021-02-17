b1c4fcd249c985b09075ad23ea1ea5325095892f
Johannes Koch <johannes.koch@avenga.com>
2021-02-17 13:43:58 +0100

    config: JWT signing profile

--- config/definitions.go
+++ config/definitions.go
@@ -2,6 +2,7 @@ package config
 
 // Definitions represents the <Definitions> object.
 type Definitions struct {
-	BasicAuth []*BasicAuth `hcl:"basic_auth,block"`
-	JWT       []*JWT       `hcl:"jwt,block"`
+	BasicAuth         []*BasicAuth         `hcl:"basic_auth,block"`
+	JWT               []*JWT               `hcl:"jwt,block"`
+	JWTSigningProfile []*JWTSigningProfile `hcl:"jwt_signing_profile,block"`
 }
