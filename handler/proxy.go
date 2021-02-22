63a12ba7282d2f0f5e25d79f8f22433569472237
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-02-22 12:37:30 +0100

    Fix: do not send xff header

--- handler/proxy.go
+++ handler/proxy.go
@@ -236,15 +236,16 @@ func (p *Proxy) roundtrip(rw http.ResponseWriter, req *http.Request) {
 		outreq.Header.Set("Upgrade", reqUpType)
 	}
 
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		// If we aren't the first proxy retain prior
-		// X-Forwarded-For information as a comma+space
-		// separated list and fold multiple headers into one.
-		if prior, ok := outreq.Header["X-Forwarded-For"]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
-		}
-		outreq.Header.Set("X-Forwarded-For", clientIP)
-	}
+	outreq.Header.Del("X-Forwarded-For")
+	//if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+	//	// If we aren't the first proxy retain prior
+	//	// X-Forwarded-For information as a comma+space
+	//	// separated list and fold multiple headers into one.
+	//	if prior, ok := outreq.Header["X-Forwarded-For"]; ok {
+	//		clientIP = strings.Join(prior, ", ") + ", " + clientIP
+	//	}
+	//	outreq.Header.Set("X-Forwarded-For", clientIP)
+	//}
 
 	var apiValidator *OpenAPIValidator
 	if p.options.OpenAPI != nil {
