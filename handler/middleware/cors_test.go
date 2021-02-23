b71aeca2de42798b1334fd41f1b1a9b451a58516
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-02-23 13:06:47 +0100

    Fixup cors test

--- handler/middleware/cors_test.go
+++ handler/middleware/cors_test.go
@@ -199,6 +199,50 @@ func TestCORSOptions_isCorsPreflightRequest(t *testing.T) {
 	}
 }
 
+func TestCORS_IsCredentialed(t *testing.T) {
+	type testCase struct {
+		name           string
+		requestHeaders map[string]string
+		exp            bool
+	}
+
+	tests := []testCase{
+		{
+			"Cookie",
+			map[string]string{"Cookie": "a=b"},
+			true,
+		},
+		{
+			"Authorization",
+			map[string]string{"Authorization": "Basic qeinbqtpoib"},
+			true,
+		},
+		{
+			"Proxy-Authorization",
+			map[string]string{"Proxy-Authorization": "Basic qeinbqtpoib"},
+			true,
+		},
+		{
+			"Not credentialed",
+			map[string]string{},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, "http://1.2.3.4/", nil)
+			for name, value := range tt.requestHeaders {
+				req.Header.Set(name, value)
+			}
+
+			credentialed := NewCORSHandler(nil).(*CORS).isCredentialed(req.Header)
+			if credentialed != tt.exp {
+				t.Errorf("expected: %t, got: %t", tt.exp, credentialed)
+			}
+		})
+	}
+}
+
 func TestCORS_ServeHTTP(t *testing.T) {
 	upstreamHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.Header().Set("Content-Type", "text/plain")
