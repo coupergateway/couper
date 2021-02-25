3149ad7a72f15c3b35e9abfdb44d5e49885a77ea
Johannes Koch <johannes.koch@avenga.com>
2021-02-25 17:39:03 +0100

    SAML2 definition config

---
+++ config/saml2.go
@@ -0,0 +1,9 @@
+package config
+
+type SAML2 struct {
+	ArrayAttributes []string `hcl:"array_attributes,optional"`
+	IdpMetadataFile string   `hcl:"idp_metadata_file"`
+	Name            string   `hcl:"name,label"`
+	SpAcsUrl        string   `hcl:"sp_acs_url"`
+	SpEntityId      string   `hcl:"sp_entity_id"`
+}
