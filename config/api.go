ec58e759e7f6c13c52f19eaffcb4396249e36233
Alex Schneider <alex.schneider@sevenval.com>
2021-03-11 11:19:05 +0100

    Config

--- config/api.go
+++ config/api.go
@@ -3,8 +3,8 @@ package config
 // API represents the <API> object.
 type API struct {
 	AccessControl        []string  `hcl:"access_control,optional"`
-	CORS                 *CORS     `hcl:"cors,block"`
 	BasePath             string    `hcl:"base_path,optional"`
+	CORS                 *CORS     `hcl:"cors,block"`
 	DisableAccessControl []string  `hcl:"disable_access_control,optional"`
 	Endpoints            Endpoints `hcl:"endpoint,block"`
 	ErrorFile            string    `hcl:"error_file,optional"`
