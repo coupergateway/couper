7aa47597aef18b531d762d3b606aa276db6f35a2
Alex Schneider <alex.schneider@avenga.com>
2021-03-16 14:43:58 +0100

    Add new strip-secure-cookies setting (#147)
    * Add new secure_cookies=strip setting
    
    * Tests
    
    * Docu
    
    * Add comment
    
    * wording change
    
    Co-authored-by: Alex Schneider <alex.schneider@sevenval.com>
    Co-authored-by: Johannes Koch <johannes.koch@avenga.com>

---
+++ server/cookies.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+const (
+	SecureCookiesStrip = "strip"
+	SecureCookieAV     = "Secure"
+	setCookieHeader    = "Set-Cookie"
+)
+
+var regexSplitSetCookie = regexp.MustCompile(`([^;]+);?`)
+
+func stripSecureCookies(header http.Header) {
+	list := header.Values(setCookieHeader)
+	header.Del(setCookieHeader)
+
+	for _, original := range list {
+		parts, isSecure := parseSetCookieHeader(original)
+
+		if !isSecure {
+			header.Add(setCookieHeader, original) // Unchanged
+		} else {
+			header.Add(setCookieHeader, strings.Join(parts, "; "))
+		}
+	}
+}
+
+// parseSetCookieHeader splits the given Set-Cookie HTTP header field value
+// and always removes the <Secure> flag. If the <Secure> flag was present, the
+// second return value is set to <true>, otherwise to <false>.
+func parseSetCookieHeader(setCookie string) ([]string, bool) {
+	var parts []string
+	var isSecure bool
+
+	for _, m := range regexSplitSetCookie.FindAllStringSubmatch(setCookie, -1) {
+		part := strings.TrimSpace(m[1])
+
+		if strings.ToLower(part) == strings.ToLower(SecureCookieAV) {
+			isSecure = true
+
+			continue
+		}
+
+		parts = append(parts, part)
+	}
+
+	return parts, isSecure
+}
