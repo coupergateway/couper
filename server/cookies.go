88a44a63bce0ac94cea93d1313545d4a3cd4a3d6
Alex Schneider <alex.schneider@sevenval.com>
2021-03-16 10:26:12 +0100

    Add new secure_cookies=strip setting

---
+++ server/cookies.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+const (
+	SecureCookiesStrip = "strip"
+	SecureCookieAV     = "Secure"
+	setCookieHeader    = "Set-Cookie"
+)
+
+var regexSplitSetCookie = regexp.MustCompile(`([^;]+);?`)
+
+func stripSecureCookies(header http.Header) {
+	list := header.Values(setCookieHeader)
+	header.Del(setCookieHeader)
+
+	for _, original := range list {
+		parts, isSecure := parseSetCookieHeader(original)
+
+		if !isSecure {
+			header.Add(setCookieHeader, original) // Unchanged
+		} else {
+			header.Add(setCookieHeader, strings.Join(parts, "; "))
+		}
+	}
+}
+
+func parseSetCookieHeader(setCookie string) ([]string, bool) {
+	var parts []string
+	var isSecure bool
+
+	for _, m := range regexSplitSetCookie.FindAllStringSubmatch(setCookie, -1) {
+		part := strings.TrimSpace(m[1])
+
+		if strings.ToLower(part) == strings.ToLower(SecureCookieAV) {
+			isSecure = true
+
+			continue
+		}
+
+		parts = append(parts, part)
+	}
+
+	return parts, isSecure
+}
