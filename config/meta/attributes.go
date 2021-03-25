487dbdebd94b18aaf9767ce4e077c3f1cce0642a
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-03-25 19:19:31 +0100

    Revert combine due to impliedbodyschema issue

--- config/meta/attributes.go
+++ config/meta/attributes.go
@@ -9,8 +9,18 @@ var AttributesSchema, _ = gohcl.ImpliedBodySchema(&Attributes{})
 
 // Attributes are commonly shared attributes which gets evaluated during runtime.
 type Attributes struct {
-	RequestAttributes
-	ResponseAttributes
+	// RequestAttributes
+	AddQueryParams    map[string]cty.Value `hcl:"add_query_params,optional"`
+	AddRequestHeaders map[string]string    `hcl:"add_request_headers,optional"`
+	DelQueryParams    []string             `hcl:"remove_query_params,optional"`
+	DelRequestHeaders []string             `hcl:"remove_request_headers,optional"`
+	Path              string               `hcl:"path,optional"`
+	SetQueryParams    map[string]cty.Value `hcl:"set_query_params,optional"`
+	SetRequestHeaders map[string]string    `hcl:"set_request_headers,optional"`
+	// ResponseAttributes
+	AddResponseHeaders map[string]string `hcl:"add_response_headers,optional"`
+	DelResponseHeaders []string          `hcl:"remove_response_headers,optional"`
+	SetResponseHeaders map[string]string `hcl:"set_response_headers,optional"`
 }
 
 type RequestAttributes struct {
