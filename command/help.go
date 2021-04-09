93c4862e29801be5aa21e232b939909dc261c8db
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-04-09 16:02:31 +0200

    Add flagSet usage calls and help command

--- command/help.go
+++ command/help.go
@@ -1,18 +1,47 @@
 package command
 
-// Help shows available commands and options.
-func Help() { // TODO: generate from command and options list
-	println(`couper usage:
+import (
+	"context"
+	"fmt"
 
-couper <cmd> <options>
+	"github.com/avenga/couper/config"
+	"github.com/sirupsen/logrus"
+)
 
-global options:
+var _ Cmd = &Help{}
 
-	-f		couper hcl configuration file
-	-log-format	format option for json or common logs
+// Synopsis shows available commands and options.
+func Synopsis() { // TODO: generate from command and options list
+	println(`Couper usage:
 
-available commands:
+  couper <cmd> <options>
 
-	run		starts the server
+Available commands:
+
+  run		Start the server with given configuration file.
+  help		Usage for given command.
+  version	Print the current version and build information.
 `)
+}
+
+type Help struct {
+	ctx context.Context
+}
+
+func NewHelp(ctx context.Context) *Help {
+	return &Help{ctx: ctx}
+}
+
+func (h Help) Execute(args Args, _ *config.Couper, _ *logrus.Entry) error {
+	if len(args) == 0 {
+		h.Usage()
+		return fmt.Errorf("missing command argument")
+	}
+	NewCommand(h.ctx, args[0]).Usage()
+	return nil
+}
+
+func (h Help) Usage() {
+	println("Usage of help:\n  help <command>	Print usage information of given command.")
+	Synopsis()
 }
