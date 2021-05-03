48e44effc831abb27f21c57e2ee37ecb204eedec
Alex Schneider <alex.schneider@avenga.com>
2021-05-03 14:11:02 +0200

    Move options tests to own test-function

--- server/http_oauth2_test.go
+++ server/http_oauth2_test.go
@@ -21,29 +21,6 @@ func TestEndpoints_OAuth2(t *testing.T) {
 
 		oauthOrigin := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			if req.URL.Path == "/oauth2" {
-				reqBody, _ := ioutil.ReadAll(req.Body)
-				authorization := req.Header.Get("Authorization")
-
-				if i == 0 {
-					exp := `client_id=user&client_secret=pass+word&grant_type=client_credentials&scope=scope1+scope2`
-					if exp != string(reqBody) {
-						t.Errorf("want\n%s\ngot\n%s", exp, reqBody)
-					}
-					exp = ""
-					if exp != authorization {
-						t.Errorf("want\n%s\ngot\n%s", exp, authorization)
-					}
-				} else {
-					exp := `grant_type=client_credentials`
-					if exp != string(reqBody) {
-						t.Errorf("want\n%s\ngot\n%s", exp, reqBody)
-					}
-					exp = "Basic dXNlcjpwYXNz"
-					if exp != authorization {
-						t.Errorf("want\n%s\ngot\n%s", exp, authorization)
-					}
-				}
-
 				rw.Header().Set("Content-Type", "application/json")
 				rw.WriteHeader(http.StatusOK)
 
@@ -128,6 +105,91 @@ func TestEndpoints_OAuth2(t *testing.T) {
 
 		oauthOrigin.Close()
 		ResourceOrigin.Close()
+		shutdown()
+	}
+}
+
+func TestEndpoints_OAuth2_Options(t *testing.T) {
+	helper := test.New(t)
+
+	type testCase struct {
+		configFile string
+		expBody    string
+		expAuth    string
+	}
+
+	for _, tc := range []testCase{
+		{
+			"01_couper.hcl",
+			`client_id=user&client_secret=pass+word&grant_type=client_credentials&scope=scope1+scope2`,
+			"",
+		},
+		{
+			"02_couper.hcl",
+			`grant_type=client_credentials`,
+			"Basic dXNlcjpwYXNz",
+		},
+		{
+			"03_couper.hcl",
+			`grant_type=client_credentials`,
+			"Basic dXNlcjpwYXNz",
+		},
+	} {
+		var tokenSeenCh chan struct{}
+
+		oauthOrigin := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			if req.URL.Path == "/options" {
+				reqBody, _ := ioutil.ReadAll(req.Body)
+				authorization := req.Header.Get("Authorization")
+
+				if tc.expBody != string(reqBody) {
+					t.Errorf("want\n%s\ngot\n%s", tc.expBody, reqBody)
+				}
+				if tc.expAuth != authorization {
+					t.Errorf("want\n%s\ngot\n%s", tc.expAuth, authorization)
+				}
+
+				rw.WriteHeader(http.StatusNoContent)
+
+				close(tokenSeenCh)
+				return
+			}
+			rw.WriteHeader(http.StatusBadRequest)
+		}))
+		defer oauthOrigin.Close()
+
+		confPath := fmt.Sprintf("testdata/oauth2/%s", tc.configFile)
+		shutdown, hook := newCouper(confPath, test.New(t))
+		defer func() {
+			if t.Failed() {
+				for _, e := range hook.Entries {
+					println(e.String())
+				}
+			}
+			shutdown()
+		}()
+
+		req, err := http.NewRequest(http.MethodGet, "http://anyserver:8080/", nil)
+		helper.Must(err)
+
+		req.Header.Set("X-Token-Endpoint", oauthOrigin.URL)
+
+		hook.Reset()
+
+		tokenSeenCh = make(chan struct{})
+
+		req.URL.Path = "/"
+		_, err = newClient().Do(req)
+		helper.Must(err)
+
+		timer := time.NewTimer(time.Second * 2)
+		select {
+		case <-timer.C:
+			t.Error("OAuth2 request failed")
+		case <-tokenSeenCh:
+		}
+
+		oauthOrigin.Close()
 		shutdown()
 	}
 }
