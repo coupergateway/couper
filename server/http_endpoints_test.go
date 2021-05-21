7f8cb0b75a59a4e5407172a721945ed6bc85aeaf
Felix Hassert <felix.hassert@avenga.com>
2021-05-21 09:14:38 +0200

    refine test for #233
    proof that all actions after the error (set_response_headers…) will not
    run

--- server/http_endpoints_test.go
+++ server/http_endpoints_test.go
@@ -381,4 +381,18 @@ func TestEndpoints_DoNotExecuteResponseOnErrors(t *testing.T) {
 	if !bytes.Contains(resBytes, []byte("<html>configuration error</html>")) {
 		t.Errorf("Expected body '<html>configuration error</html>', given '%s'", resBytes)
 	}
+
+	// header from error handling is set
+	if v := res.Header.Get("couper-error"); v != "configuration error" {
+		t.Errorf("want couper-error 'configuration error', got %q", v)
+	}
+
+	// happy path headers not set
+	if res.Header.Get("x-backend") != "" {
+		t.Errorf("backend.set_response_headers should not have been run")
+	}
+
+	if res.Header.Get("x-endpoint") != "" {
+		t.Errorf("endpoint.set_response_headers should not have been run")
+	}
 }
