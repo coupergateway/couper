d8edc64289bb6ece89e2b3d79b303dedf7a7cadd
Alex Schneider <alex.schneider@avenga.com>
2021-06-16 13:14:49 +0200

    Draft

--- server/writer/gzip.go
+++ server/writer/gzip.go
@@ -2,6 +2,7 @@ package writer
 
 import (
 	"bufio"
+	"bytes"
 	"compress/gzip"
 	"fmt"
 	"net"
@@ -15,6 +16,8 @@ const (
 	ContentLengthHeader   = "Content-Length"
 	GzipName              = "gzip"
 	VaryHeader            = "Vary"
+
+	minCompressBodyLenght = 60
 )
 
 var (
@@ -24,21 +27,57 @@ var (
 )
 
 type Gzip struct {
-	enabled bool
-	rw      http.ResponseWriter
-	w       *gzip.Writer
+	buffer     *bytes.Buffer
+	enabled    bool
+	headerSent bool
+	statusCode int
+	writeErr   error
+	rw         http.ResponseWriter
+	w          *gzip.Writer
 }
 
 func NewGzipWriter(rw http.ResponseWriter, header http.Header) *Gzip {
-	gw := gzip.NewWriter(rw)
 	return &Gzip{
+		buffer:  bytes.NewBuffer(nil),
 		enabled: clientSupportsGZ.MatchString(header.Get(AcceptEncodingHeader)),
 		rw:      rw,
-		w:       gw,
+		w:       gzip.NewWriter(rw),
 	}
 }
 
 func (g *Gzip) Write(p []byte) (n int, err error) {
+	bytesLen := len(p)
+	bufLen := g.buffer.Len()
+
+	if bufLen < minCompressBodyLenght {
+		cap := minCompressBodyLenght - bufLen
+
+		if bytesLen < cap {
+			return g.buffer.Write(p)
+		}
+
+		// Fill the buffer at least to minCompressBodyLenght size.
+		_, err := g.buffer.Write(p)
+		if err != nil {
+			return 0, err
+		}
+
+		p = g.buffer.Bytes()
+	}
+
+	g.writeHeader()
+
+	l, err := g.write(p)
+	if err != nil {
+		return l, err
+	} else if bufLen < minCompressBodyLenght && bytesLen != (l-bufLen) {
+		return 0, fmt.Errorf("invalid write result")
+	}
+
+	return bytesLen, err
+}
+
+func (g *Gzip) write(p []byte) (n int, err error) {
 	if g.enabled {
 		return g.w.Write(p)
 	}
@@ -46,9 +85,26 @@ func (g *Gzip) Write(p []byte) (n int, err error) {
 }
 
 func (g *Gzip) Close() (err error) {
+	if g.writeErr != nil {
+		return g.writeErr
+	}
+
+	if g.buffer.Len() < minCompressBodyLenght {
+		g.enabled = false
+		g.writeHeader()
+
+		_, err := g.write(g.buffer.Bytes())
+		if err != nil {
+			return err
+		}
+	}
+
+	g.writeHeader()
+
 	if g.enabled && g.w != nil {
 		err = g.w.Close()
 	}
+
 	return err
 }
 
@@ -57,17 +113,44 @@ func (g *Gzip) Header() http.Header {
 }
 
 func (g *Gzip) WriteHeader(statusCode int) {
-	g.rw.Header().Add(VaryHeader, AcceptEncodingHeader)
+	g.statusCode = statusCode
+}
+
+func (g *Gzip) writeHeader() {
+	if g.headerSent {
+		return
+	}
+
+	g.headerSent = true
+
+	if g.buffer.Len() >= minCompressBodyLenght {
+		g.rw.Header().Add(VaryHeader, AcceptEncodingHeader)
+	}
 
 	if g.enabled {
 		g.rw.Header().Del(ContentLengthHeader)
 		g.rw.Header().Set(ContentEncodingHeader, GzipName)
 	}
 
-	g.rw.WriteHeader(statusCode)
+	g.rw.WriteHeader(g.statusCode)
 }
 
 func (g *Gzip) Flush() {
+	if l := g.buffer.Len(); l < minCompressBodyLenght {
+		g.enabled = false
+		g.writeHeader()
+
+		_, g.writeErr = g.write(g.buffer.Bytes())
+
+		// Fill the buffer up to minCompressBodyLenght size.
+		_, err := g.buffer.Write(make([]byte, minCompressBodyLenght-l))
+		if g.writeErr == nil {
+			g.writeErr = err
+		}
+	}
+
+	g.writeHeader()
+
 	if g.enabled && g.w != nil {
 		_ = g.w.Flush()
 	}
