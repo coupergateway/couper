a93715577c8dcf3c28e43970dbc92937919ddf7e
Christopher Overdiek <85127559+Buphido@users.noreply.github.com>
2021-09-09 12:35:28 +0200

    Log documentation (#310)
    * Removed warnings from http_oauth2_test.go
    
    * Added rudimentary LOGS.md
    
    * Changes to read and utilize previously removed err
    
    * added table of contents, adjusted changelog
    
    * Progress in LOGS.md, small fixes in CLI.md
    
    * Finished LOGS.md first draft
    
    * first changes to LOGS, fixed bug in upstream_log
    
    * Added reference for origin
    
    * Update docs/LOGS.md
    
    Co-authored-by: vwuerbel <vwuerbel@gmail.com>
    
    * Update docs/LOGS.md

--- server/http_oauth2_test.go
+++ server/http_oauth2_test.go
@@ -349,6 +349,8 @@ func TestOAuth2_AccessControl(t *testing.T) {
 			}
 
 			tokenResBytes, err := io.ReadAll(res.Body)
+			helper.Must(err)
+
 			var jData map[string]interface{}
 			json.Unmarshal(tokenResBytes, &jData)
 			if params, ok := jData["form_params"]; ok {
@@ -356,16 +358,16 @@ func TestOAuth2_AccessControl(t *testing.T) {
 					subT.Errorf("%q: expected params %s, got: %s", tc.name, tc.params, params)
 				}
 			} else {
-				if "" != tc.params {
+				if tc.params != "" {
 					subT.Errorf("%q: expected params %s, got no", tc.name, tc.params)
 				}
 			}
 			if authorization, ok := jData["authorization"]; ok {
-				if authorization != tc.authorization {
+				if tc.authorization != authorization {
 					subT.Errorf("%q: expected authorization %s, got: %s", tc.name, tc.authorization, authorization)
 				}
 			} else {
-				if "" != tc.authorization {
+				if tc.authorization != "" {
 					subT.Errorf("%q: expected authorization %s, got no", tc.name, tc.authorization)
 				}
 			}
