560b8052265eb01bb4957b07288ad93586cc6cfe
Buphido <christopher@overdiek.de>
2021-09-13 18:42:45 +0200

    Attempted implementation of backend_probes.<name>

--- handler/transport/probe_map/probe_map.go
+++ handler/transport/probe_map/probe_map.go
@@ -8,12 +8,12 @@ type BackendProbes map[string]string
 
 var (
 	probeMutex       sync.RWMutex
-	backendProbesMap = make(BackendProbes)
+	BackendProbesMap = make(BackendProbes)
 )
 
 func SetBackendProbe(name string, state string) {
 	probeMutex.Lock()
-	backendProbesMap[name] = state
+	BackendProbesMap[name] = state
 	probeMutex.Unlock()
 }
 
@@ -21,7 +21,7 @@ func GetBackendProbes() BackendProbes {
 	cp := make(BackendProbes)
 
 	probeMutex.RLock()
-	for name, state := range backendProbesMap {
+	for name, state := range BackendProbesMap {
 		cp[name] = state
 	}
 	probeMutex.RUnlock()
