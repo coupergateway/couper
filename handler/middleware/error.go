5e372c9ca19fd65e5cb6c67e194cc26b4089396b
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-10-11 12:59:30 +0200

    Fixup beta_scope error types
    Base kind missing, chained kinds must contain base name

--- handler/middleware/error.go
+++ handler/middleware/error.go
@@ -13,7 +13,8 @@ func NewErrorHandler(condition condition, eh http.Handler) Next {
 	return func(handler http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			if err := condition(req); err != nil {
-				eh.ServeHTTP(rw, req.WithContext(context.WithValue(req.Context(), request.Error, err)))
+				*req = *req.WithContext(context.WithValue(req.Context(), request.Error, err))
+				eh.ServeHTTP(rw, req)
 				return
 			}
 			handler.ServeHTTP(rw, req)
