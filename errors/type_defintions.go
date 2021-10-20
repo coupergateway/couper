4b8b6c1c5a0accd4268de8f2e34722735b40d62a
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-10-20 11:21:39 +0200

    fmt

--- errors/type_defintions.go
+++ errors/type_defintions.go
@@ -16,9 +16,9 @@ var Definitions = []*Error{
 	AccessControl.Kind("oauth2"),
 
 	AccessControl.Kind("saml2"),
-	
+
 	AccessControl.Kind("beta_operation_denied"),
-	
+
 	AccessControl.Kind("beta_scope"),
 	AccessControl.Kind("beta_scope").Kind("beta_insufficient_scope"),
 }
