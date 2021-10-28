fe6debc51b049bf24b8b0c1824b4746bdb1be9a3
Alex Schneider <alex.schneider@avenga.com>
2021-10-28 16:11:08 +0200

    Fix test

--- eval/lib/url_test.go
+++ eval/lib/url_test.go
@@ -38,7 +38,7 @@ func TestUrlEncode(t *testing.T) {
 func TestRelativeUrl(t *testing.T) {
 	helper := test.New(t)
 
-	cf, err := configload.LoadBytes([]byte(`server "test" {}`), "couper.hcl")
+	cf, err := configload.LoadBytes([]byte(`server "test" {}`), "couper.hcl", false)
 	helper.Must(err)
 
 	hclContext := cf.Context.Value(request.ContextType).(*eval.Context).HCLContext()
