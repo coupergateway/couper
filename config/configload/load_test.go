0d8db5ae9c88528925985006ee1308af4fd2cf8b
Alex Schneider <alex.schneider@avenga.com>
2021-10-29 10:49:03 +0200

    Fix: Use the referenced backend

--- config/configload/load_test.go
+++ config/configload/load_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func Test_refineEndpoints_noPattern(t *testing.T) {
-	err := refineEndpoints(nil, config.Endpoints{{Pattern: ""}}, true)
+	err := refineEndpoints(nil, nil, config.Endpoints{{Pattern: ""}}, true)
 	if err == nil || !strings.HasSuffix(err.Error(), "endpoint: missing path pattern; ") {
 		t.Errorf("refineEndpoints() error = %v, wantErr: endpoint: missing path pattern ", err)
 	}
