12a0138b3b87b5022e9ab106525ff3ede786baef
Alex Schneider <alex.schneider@avenga.com>
2021-11-23 15:10:52 +0100

    Tests

---
+++ server/http_custom_logs_test.go
@@ -0,0 +1,163 @@
+package server_test
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/avenga/couper/internal/test"
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
+)
+
+func TestCustomLogs_Upstream(t *testing.T) {
+	client := newClient()
+	helper := test.New(t)
+
+	shutdown, hook := newCouper("testdata/integration/logs/01_couper.hcl", test.New(t))
+	defer shutdown()
+
+	type testCase struct {
+		path  string
+		expAL logrus.Fields
+		expUL logrus.Fields
+	}
+
+	for _, tc := range []testCase{
+		{
+			"/",
+			logrus.Fields{
+				"api":      "couper test-backend",
+				"endpoint": "couper test-backend",
+				"server":   "couper test-backend",
+			},
+			logrus.Fields{
+				"array": []interface{}{
+					float64(1),
+					"couper test-backend",
+					[]interface{}{
+						float64(2),
+						"couper test-backend",
+					},
+					logrus.Fields{"x": "X"},
+				},
+				"bool":   true,
+				"float":  1.23,
+				"int":    float64(123),
+				"object": logrus.Fields{"a": "A", "b": "B", "c": float64(123)},
+				"string": "couper test-backend",
+			},
+		},
+		{
+			"/backend",
+			logrus.Fields{"api": "couper test-backend", "server": "couper test-backend"},
+			logrus.Fields{"backend": string("couper test-backend")},
+		},
+	} {
+		req, err := http.NewRequest(http.MethodGet, "http://localhost:8080"+tc.path, nil)
+		helper.Must(err)
+
+		hook.Reset()
+		_, err = client.Do(req)
+		helper.Must(err)
+
+		// Wait for logs
+		time.Sleep(200 * time.Millisecond)
+
+		// Access log
+		got, ok := hook.AllEntries()[1].Data["custom"].(logrus.Fields)
+		if !ok {
+			t.Fatalf("expected\n%#v\ngot\n%#v", tc.expAL, got)
+		}
+		if !reflect.DeepEqual(tc.expAL, got) {
+			t.Errorf("expected\n%#v\ngot\n%#v", tc.expAL, got)
+		}
+
+		// Upstream log
+		got, ok = hook.AllEntries()[0].Data["custom"].(logrus.Fields)
+		if !ok {
+			t.Fatalf("expected\n%#v\ngot\n%#v", tc.expUL, got)
+		}
+		if !cmp.Equal(tc.expUL, got) {
+			t.Fail()
+
+			fmt.Println(cmp.Diff(tc.expUL, got))
+		}
+	}
+}
+
+func TestCustomLogs_Local(t *testing.T) {
+	client := newClient()
+	helper := test.New(t)
+
+	shutdown, hook := newCouper("testdata/integration/logs/01_couper.hcl", test.New(t))
+	defer shutdown()
+
+	type testCase struct {
+		path string
+		exp  logrus.Fields
+	}
+
+	for _, tc := range []testCase{
+		{"/secure", logrus.Fields{"error_handler": "GET"}},
+		{"/file.html", logrus.Fields{"files": "GET"}},
+		{"/spa", logrus.Fields{"spa": "GET"}},
+	} {
+		req, err := http.NewRequest(http.MethodGet, "http://localhost:8080"+tc.path, nil)
+		helper.Must(err)
+
+		hook.Reset()
+		_, err = client.Do(req)
+		helper.Must(err)
+
+		// Wait for logs
+		time.Sleep(200 * time.Millisecond)
+
+		// Access log
+		got, ok := hook.AllEntries()[0].Data["custom"].(logrus.Fields)
+		if !ok {
+			t.Fatalf("expected\n%#v\ngot\n%#v", tc.exp, got)
+		}
+		if !reflect.DeepEqual(tc.exp, got) {
+			t.Errorf("expected\n%#v\ngot\n%#v", tc.exp, got)
+		}
+	}
+}
+
+func TestCustomLogs_Merge(t *testing.T) {
+	client := newClient()
+	helper := test.New(t)
+
+	shutdown, hook := newCouper("testdata/integration/logs/02_couper.hcl", test.New(t))
+	defer shutdown()
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	helper.Must(err)
+
+	hook.Reset()
+	_, err = client.Do(req)
+	helper.Must(err)
+
+	// Wait for logs
+	time.Sleep(200 * time.Millisecond)
+
+	exp := logrus.Fields{
+		"api":      true,
+		"endpoint": true,
+		"l1":       "endpoint",
+		"l2":       []interface{}{"server", "api", "endpoint"},
+		"l3":       []interface{}{"endpoint"},
+		"server":   true,
+	}
+
+	// Access log
+	got, ok := hook.AllEntries()[0].Data["custom"].(logrus.Fields)
+	if !ok {
+		t.Fatalf("expected\n%#v\ngot\n%#v", exp, got)
+	}
+	if !reflect.DeepEqual(exp, got) {
+		t.Errorf("expected\n%#v\ngot\n%#v", exp, got)
+	}
+}
---
+++ server/testdata/integration/logs/01_couper.hcl
@@ -0,0 +1,96 @@
+server "logs" {
+  files {
+    document_root = "./"
+    custom_log_fields = {
+      files = request.method
+    }
+  }
+
+  spa {
+    bootstrap_file = "./file.html"
+    paths = ["/spa/**"]
+    custom_log_fields = {
+      spa = request.method
+    }
+  }
+
+  custom_log_fields = {
+    server = backend_responses.default.headers.server
+  }
+
+  endpoint "/secure" {
+    access_control = ["BA"]
+
+    proxy {
+      backend = "BE"
+    }
+  }
+
+  api {
+    custom_log_fields = {
+      api = backend_responses.default.headers.server
+    }
+
+    endpoint "/" {
+      custom_log_fields = {
+        endpoint = backend_responses.default.headers.server
+      }
+
+      proxy {
+        backend "BE" {
+          custom_log_fields = {
+            bool   = true
+            int    = 123
+            float  = 1.23
+            string = backend_responses.default.headers.server
+
+            array = [
+              1,
+              backend_responses.default.headers.server,
+              [
+                2,
+                backend_responses.default.headers.server
+              ],
+              {
+                x = "X"
+              }
+            ]
+
+            object = {
+              a = "A"
+              b = "B"
+              c = 123
+            }
+          }
+        }
+      }
+    }
+
+    endpoint "/backend" {
+      proxy {
+        backend = "BE"
+      }
+    }
+  }
+}
+
+definitions {
+  backend "BE" {
+    origin = env.COUPER_TEST_BACKEND_ADDR
+    path   = "/anything"
+
+    custom_log_fields = {
+      backend = backend_responses.default.headers.server
+    }
+  }
+
+  basic_auth "BA" {
+    password = "secret"
+
+    error_handler "basic_auth" {
+      custom_log_fields = {
+        error_handler = request.method
+      }
+    }
+  }
+}
