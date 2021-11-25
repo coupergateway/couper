5be9e18dc740fa45aa9f3903eb597f17aed1c6fc
Marcel Ludwig <marcel.ludwig@avenga.com>
2021-11-25 18:04:51 +0100

    Fix BuildDate string, time.now overrides ldflag string; prepare semver v removal

--- utils/version.go
+++ utils/version.go
@@ -3,7 +3,18 @@ package utils
 import "time"
 
 var (
-	BuildDate   = time.Now().Format("2006-01-02")
+	BuildDate   = ""
 	BuildName   = "dev"
 	VersionName = "0"
 )
+
+func init() {
+	if BuildDate == "" {
+		BuildDate = time.Now().Format("2006-01-02")
+	}
+
+	// strip out possible semver v
+	if len(VersionName) > 0 && VersionName[0] == 'v' {
+		VersionName = VersionName[1:]
+	}
+}
