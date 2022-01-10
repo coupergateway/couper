e0cf0e46a0e34d5ac623c11ae8abfc0ca1917fd6
Marcel Ludwig <marcel.ludwig@avenga.com>
2022-01-10 14:55:27 +0100

    (ci) add sha256 sum to bin release; prepare osx tpl brew formula (#420)
    * add sha256 sum to bin release; prepare osx tpl brew formula
    
    * add formula update step

---
+++ .github/workflows/couper.rb
@@ -0,0 +1,47 @@
+class Couper < Formula
+    desc "Couper is a lightweight open source API gateway designed to support developers in building and running API-driven Web projects."
+    homepage "https://couper.io/"
+    license "MIT"
+    version "{{ GITHUB_REF_NAME }}"
+    head "https://github.com/avenga/couper.git", branch: "master"
+
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/avenga/couper/releases/download/{{ GITHUB_REF_NAME }}/couper-{{ GITHUB_REF_NAME }}-macos-arm64.zip"
+        sha256 "{{ MACOS_ARM64_SHA256 }}"
+
+        def install
+          bin.install "couper"
+        end
+      end
+      if Hardware::CPU.intel?
+        url "https://github.com/avenga/couper/releases/download/{{ GITHUB_REF_NAME }}/couper-{{ GITHUB_REF_NAME }}-macos-amd64.zip"
+        sha256 "{{ MACOS_AMD64_SHA256 }}"
+
+        def install
+          bin.install "couper"
+        end
+      end
+    end
+
+    on_linux do
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url "https://github.com/avenga/couper/releases/download/{{ GITHUB_REF_NAME }}/couper-{{ GITHUB_REF_NAME }}-linux-arm64.zip"
+
+        def install
+          bin.install "couper"
+        end
+      end
+      if Hardware::CPU.intel?
+        url "https://github.com/avenga/couper/releases/download/{{ GITHUB_REF_NAME }}/couper-{{ GITHUB_REF_NAME }}-linux-amd64.zip"
+
+        def install
+          bin.install "couper"
+        end
+      end
+    end
+
+    test do
+      system "#{bin}/couper version"
+    end
+  end
