0bc97b7e0c6b4c03451d4fffefcd3754404a14bc
Marcel Ludwig <marcel.ludwig@avenga.com>
2022-02-28 18:59:25 +0100

    Revert "Refactor syncJson; use channels to ensure existing data"
    This reverts commit f1ae8c614a5cc3c581d31db1ec5db04f0a50d58f.

--- json/synced_json.go
+++ json/synced_json.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/avenga/couper/config/reader"
@@ -16,99 +17,89 @@ type SyncedJSONUnmarshaller interface {
 }
 
 type SyncedJSON struct {
-	roundTripName string
+	file          string
+	fileContext   string
+	uri           string
 	transport     http.RoundTripper
+	roundTripName string
 	ttl           time.Duration
 	unmarshaller  SyncedJSONUnmarshaller
-	uri           string
 	// used internally
-	data     interface{}
-	exchange chan chan interface{}
+	data   interface{}
+	expiry int64
+	mtx    sync.RWMutex
 }
 
 func NewSyncedJSON(file, fileContext, uri string, transport http.RoundTripper, roundTripName string, ttl time.Duration, unmarshaller SyncedJSONUnmarshaller) (*SyncedJSON, error) {
 	sj := &SyncedJSON{
-		exchange:      make(chan chan interface{}),
-		roundTripName: roundTripName,
+		file:          file,
+		fileContext:   fileContext,
+		uri:           uri,
 		transport:     transport,
+		roundTripName: roundTripName,
 		ttl:           ttl,
 		unmarshaller:  unmarshaller,
-		uri:           uri,
 	}
-
-	if file != "" {
-		if err := sj.readFile(fileContext, file); err != nil {
-			return nil, err
-		}
-	} else if transport != nil {
-		go sj.sync(context.Background()) // TODO: at least cmd cancel (reload)
-	} else {
-		return nil, fmt.Errorf("synced JSON: missing both file and request")
-	}
-
-	return sj, nil
-}
-
-func (s *SyncedJSON) sync(ctx context.Context) {
-	expired := time.After(time.Millisecond) // force initial fetch()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-expired:
-			err := s.fetch()
-			if err != nil {
-				time.Sleep(time.Second * 10)
-				continue
-			}
-			expired = time.After(s.ttl)
-		case dataRequest := <-s.exchange:
-			if s.data == nil { // initial race
-				_ = s.fetch()
-			}
-			dataRequest <- s.data
-		}
-	}
+	_, err := sj.Data("")
+	return sj, err
 }
 
 // Data must block all other requests.
 func (s *SyncedJSON) Data(uid string) (interface{}, error) {
-	dataRequest := make(chan interface{})
-	s.exchange <- dataRequest
-	return <-dataRequest, nil
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	var rawJSON []byte
+
+	if s.data != nil && !s.hasExpired() { // edge case if someone hangs at this methods lock
+		return s.data, nil
+	} else if s.file != "" {
+		j, err := reader.ReadFromFile(s.fileContext, s.file)
+		if err != nil {
+			return nil, err
+		}
+		rawJSON = j
+	} else if s.transport != nil {
+		req, err := http.NewRequest("GET", "", nil)
+		if err != nil {
+			return nil, err
+		}
+		ctx := context.WithValue(context.Background(), request.URLAttribute, s.uri)
+		ctx = context.WithValue(ctx, request.RoundTripName, s.roundTripName)
+		if uid != "" {
+			ctx = context.WithValue(ctx, request.UID, uid)
+		}
+		req = req.WithContext(ctx)
+		response, err := s.transport.RoundTrip(req)
+		if err != nil {
+			return nil, err
+		}
+		if response.StatusCode != 200 {
+			return nil, fmt.Errorf("status code %d", response.StatusCode)
+		}
+
+		defer response.Body.Close()
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return nil, fmt.Errorf("error reading response for %q: %v", s.uri, err)
+		}
+		rawJSON = body
+	} else {
+		return nil, fmt.Errorf("synced JSON: missing both file and request")
+	}
+
+	jsonData, err := s.unmarshaller.Unmarshal(rawJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	s.data = jsonData
+	s.expiry = time.Now().Unix() + int64(s.ttl.Seconds())
+
+	return jsonData, nil
 }
 
-func (s *SyncedJSON) fetch() error {
-	req, _ := http.NewRequest("GET", s.uri, nil)
-
-	ctx := context.WithValue(context.Background(), request.URLAttribute, s.uri)
-	ctx = context.WithValue(ctx, request.RoundTripName, s.roundTripName)
-
-	req = req.WithContext(ctx)
-
-	response, err := s.transport.RoundTrip(req)
-	if err != nil {
-		return err
-	}
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("status code %d", response.StatusCode)
-	}
-
-	defer response.Body.Close()
-
-	raw, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response for %q: %v", s.uri, err)
-	}
-	s.data, err = s.unmarshaller.Unmarshal(raw)
-	return err
-}
-
-func (s *SyncedJSON) readFile(context, path string) error {
-	raw, err := reader.ReadFromFile(context, path)
-	if err != nil {
-		return err
-	}
-	s.data, err = s.unmarshaller.Unmarshal(raw)
-	return err
+func (s *SyncedJSON) hasExpired() bool {
+	return time.Now().Unix() > s.expiry
 }
