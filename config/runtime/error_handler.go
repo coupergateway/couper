e6da324c2e27fe1853e4cfda7a10948e67bcad8b
Johannes Koch <johannes.koch@avenga.com>
2022-03-30 12:30:14 +0200

    go fmt

--- config/runtime/error_handler.go
+++ config/runtime/error_handler.go
@@ -49,41 +49,41 @@ func newErrorHandler(ctx *hcl.EvalContext, opts *protectedOptions, log *logrus.E
 		}
 
 		for k, h := range handlersPerKind {
-				contextBody := h.HCLBody()
+			contextBody := h.HCLBody()
 
-				epConf := &config.Endpoint{
-					Remain:    contextBody,
-					Proxies:   h.Proxies,
-					ErrorFile: h.ErrorFile,
-					Requests:  h.Requests,
-					Response:  h.Response,
-				}
+			epConf := &config.Endpoint{
+				Remain:    contextBody,
+				Proxies:   h.Proxies,
+				ErrorFile: h.ErrorFile,
+				Requests:  h.Requests,
+				Response:  h.Response,
+			}
 
-				emptyBody := hcl.EmptyBody()
-				if epConf.Response == nil { // Set dummy resp to skip related requirement checks, allowed for error_handler.
-					epConf.Response = &config.Response{Remain: emptyBody}
-				}
+			emptyBody := hcl.EmptyBody()
+			if epConf.Response == nil { // Set dummy resp to skip related requirement checks, allowed for error_handler.
+				epConf.Response = &config.Response{Remain: emptyBody}
+			}
 
-				epOpts, err := newEndpointOptions(ctx, epConf, nil, opts.srvOpts, log, opts.settings, opts.memStore)
-				if err != nil {
-					return nil, err
-				}
-				if epOpts.ErrorTemplate == nil || h.ErrorFile == "" {
-					epOpts.ErrorTemplate = opts.epOpts.ErrorTemplate
-				}
+			epOpts, err := newEndpointOptions(ctx, epConf, nil, opts.srvOpts, log, opts.settings, opts.memStore)
+			if err != nil {
+				return nil, err
+			}
+			if epOpts.ErrorTemplate == nil || h.ErrorFile == "" {
+				epOpts.ErrorTemplate = opts.epOpts.ErrorTemplate
+			}
 
-				epOpts.ErrorTemplate = epOpts.ErrorTemplate.WithContextFunc(func(rw http.ResponseWriter, r *http.Request) {
-					beresp := &http.Response{Header: rw.Header()}
-					_ = eval.ApplyResponseContext(eval.ContextFromRequest(r).HCLContextSync(), contextBody, beresp)
-				})
+			epOpts.ErrorTemplate = epOpts.ErrorTemplate.WithContextFunc(func(rw http.ResponseWriter, r *http.Request) {
+				beresp := &http.Response{Header: rw.Header()}
+				_ = eval.ApplyResponseContext(eval.ContextFromRequest(r).HCLContextSync(), contextBody, beresp)
+			})
 
-				if epOpts.Response != nil && reflect.DeepEqual(epOpts.Response.Context, emptyBody) {
-					epOpts.Response = nil
-				}
+			if epOpts.Response != nil && reflect.DeepEqual(epOpts.Response.Context, emptyBody) {
+				epOpts.Response = nil
+			}
 
-				epOpts.LogHandlerKind = "error_" + k
-				epOpts.IsErrorHandler = true
-				kindsHandler[k] = handler.NewEndpoint(epOpts, log, nil)
+			epOpts.LogHandlerKind = "error_" + k
+			epOpts.IsErrorHandler = true
+			kindsHandler[k] = handler.NewEndpoint(epOpts, log, nil)
 		}
 	}
 
