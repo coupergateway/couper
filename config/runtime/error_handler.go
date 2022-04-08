b74776278a4ed7d7a1b611bac731507940416f73
Alex Schneider <alex.schneider@avenga.com>
2022-04-08 11:03:35 +0200

    Cleanup

--- config/runtime/error_handler.go
+++ config/runtime/error_handler.go
@@ -13,8 +13,8 @@ import (
 )
 
 var wildcardMap = map[string][]string{
-	"api":      []string{"beta_insufficient_scope", "beta_operation_denied"},
-	"endpoint": []string{"beta_insufficient_scope", "beta_operation_denied", "sequence", "unexpected_status"},
+	"api":      {"beta_insufficient_scope", "beta_operation_denied"},
+	"endpoint": {"beta_insufficient_scope", "beta_operation_denied", "sequence", "unexpected_status"},
 }
 
 func newErrorHandler(ctx *hcl.EvalContext, opts *protectedOptions, log *logrus.Entry,
