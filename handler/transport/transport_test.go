83bcb50267f809c7fb930ab1bdd4b2aed0e99c80
Alex Schneider <alex.schneider@avenga.com>
2022-04-20 11:27:12 +0200

    Fix test

--- handler/transport/transport_test.go
+++ handler/transport/transport_test.go
@@ -1,13 +1,18 @@
 package transport
 
 import (
+	"context"
 	"testing"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Test_parseDuration(t *testing.T) {
+	log := logrus.New()
+
 	var target time.Duration
-	parseDuration("1ms", &target)
+	parseDuration("1ms", &target, log.WithContext(context.TODO()))
 
 	if target != 1000000 {
 		t.Errorf("Unexpected duration given: %#v", target)
