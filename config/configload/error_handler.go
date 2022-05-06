fdd957eb8d377437fe14640eb0cec99eed2c49ca
Johannes Koch <johannes.koch@avenga.com>
2022-05-06 12:42:29 +0200

    use local range variable in both cases

--- config/configload/error_handler.go
+++ config/configload/error_handler.go
@@ -75,26 +75,28 @@ func newErrorHandlerContent(content *hcl.BodyContent) (map[string]struct{}, []ki
 		}
 		for _, k := range kinds {
 			if _, exist := configuredKinds[k]; exist {
-				if len(block.LabelRanges) == 0 {
-					block.LabelRanges = append(block.LabelRanges, block.DefRange)
+				subjRange := block.DefRange
+				if len(block.LabelRanges) > 0 {
+					subjRange = block.LabelRanges[0]
 				}
 
 				return nil, nil, hcl.Diagnostics{&hcl.Diagnostic{
 					Severity: hcl.DiagError,
 					Summary:  fmt.Sprintf("duplicate error type registration: %q", k),
-					Subject:  &block.LabelRanges[0],
+					Subject:  &subjRange,
 				}}
 			}
 
 			if k != errors.Wildcard && !errors.IsKnown(k) {
-				if len(block.LabelRanges) == 0 {
-					block.LabelRanges = append(block.LabelRanges, block.DefRange)
+				subjRange := block.DefRange
+				if len(block.LabelRanges) > 0 {
+					subjRange = block.LabelRanges[0]
 				}
 
 				return nil, nil, hcl.Diagnostics{&hcl.Diagnostic{
 					Severity: hcl.DiagError,
 					Summary:  fmt.Sprintf("error type is unknown: %q", k),
-					Subject:  &block.LabelRanges[0],
+					Subject:  &subjRange,
 				}}
 			}
 
