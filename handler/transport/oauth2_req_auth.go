6ad0c2280f57ea1c95e15e66340e592fed98294d
Johannes Koch <johannes.koch@avenga.com>
2022-06-08 15:32:56 +0200

    use pointer address of backend authorizer, not of AS backend as token store key

--- handler/transport/oauth2_req_auth.go
+++ handler/transport/oauth2_req_auth.go
@@ -26,13 +26,14 @@ type OAuth2ReqAuth struct {
 // to retrieve a valid token before passing the initial out request.
 func NewOAuth2ReqAuth(conf *config.OAuth2ReqAuth, memStore *cache.MemoryStore,
 	oauth2Client *oauth2.ClientCredentialsClient) (TokenRequest, error) {
-	return &OAuth2ReqAuth{
+	reqAuth := &OAuth2ReqAuth{
 		config:       conf,
 		oauth2Client: oauth2Client,
 		memStore:     memStore,
 		locks:        sync.Map{},
-		storageKey:   fmt.Sprintf("%p|%s|%s", &oauth2Client.Backend, conf.ClientID, conf.ClientSecret),
-	}, nil
+	}
+	reqAuth.storageKey = fmt.Sprintf("oauth2-%p", reqAuth)
+	return reqAuth, nil
 }
 
 func (oa *OAuth2ReqAuth) WithToken(req *http.Request) error {
