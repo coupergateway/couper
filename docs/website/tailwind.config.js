6cc84433ca9ce61522a092121f8e511c4d76cfc5
Marcel Ludwig <marcel.ludwig@avenga.com>
2022-07-04 12:26:55 +0200

    active nav links

--- docs/website/tailwind.config.js
+++ docs/website/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      './components/**/*.vue',
+      './layouts/**/*.vue',
+  ],
   theme: {
     extend: {},
   },
