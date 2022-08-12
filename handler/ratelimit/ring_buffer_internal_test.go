6fb47172d8bdb3e71b831c524c5c83617e3e9c58
Alex Schneider <alex.schneider@avenga.com>
2022-08-12 10:04:10 +0200

    Extend ring buffer tests

--- handler/ratelimit/ring_buffer_internal_test.go
+++ handler/ratelimit/ring_buffer_internal_test.go
@@ -23,7 +23,10 @@ func TestRingBuffer_New(t *testing.T) {
 }
 
 func TestRingBuffer_Put(t *testing.T) {
-	now := time.Now()
+	now1 := time.Now()
+	now2 := time.Now()
+	now3 := time.Now()
+	now4 := time.Now()
 
 	r := newRingBuffer(3)
 
@@ -36,10 +39,13 @@ func TestRingBuffer_Put(t *testing.T) {
 	if !r.buf[2].IsZero() {
 		t.Errorf("Unexpected r.buf[2]: %#v", r.buf[2])
 	}
+	if got := r.get(); !got.IsZero() {
+		t.Errorf("Unexpected r.get(): %#v", got)
+	}
 
-	r.put(now)
+	r.put(now1)
 
-	if !r.buf[0].Equal(now) {
+	if !r.buf[0].Equal(now1) {
 		t.Errorf("Unexpected r.buf[0]: %#v", r.buf[0])
 	}
 	if !r.buf[1].IsZero() {
@@ -47,6 +53,9 @@ func TestRingBuffer_Put(t *testing.T) {
 	}
 	if !r.buf[2].IsZero() {
 		t.Errorf("Unexpected r.buf[2]: %#v", r.buf[2])
+	}
+	if got := r.get(); !got.IsZero() {
+		t.Errorf("Unexpected r.get(): %#v", got)
 	}
 
 	if r.r != 1 {
@@ -56,22 +65,67 @@ func TestRingBuffer_Put(t *testing.T) {
 		t.Errorf("Unexpected r.w: %d", r.w)
 	}
 
-	r.put(now)
+	r.put(now2)
 
-	if !r.buf[0].Equal(now) {
+	if !r.buf[0].Equal(now1) {
 		t.Errorf("Unexpected r.buf[0]: %#v", r.buf[0])
 	}
-	if !r.buf[1].Equal(now) {
+	if !r.buf[1].Equal(now2) {
 		t.Errorf("Unexpected r.buf[1]: %#v", r.buf[1])
 	}
 	if !r.buf[2].IsZero() {
 		t.Errorf("Unexpected r.buf[2]: %#v", r.buf[2])
+	}
+	if got := r.get(); !got.IsZero() {
+		t.Errorf("Unexpected r.get(): %#v", got)
 	}
 
 	if r.r != 2 {
 		t.Errorf("Unexpected r.r: %d", r.r)
 	}
 	if r.w != 1 {
+		t.Errorf("Unexpected r.w: %d", r.w)
+	}
+
+	r.put(now3)
+
+	if !r.buf[0].Equal(now1) {
+		t.Errorf("Unexpected r.buf[0]: %#v", r.buf[0])
+	}
+	if !r.buf[1].Equal(now2) {
+		t.Errorf("Unexpected r.buf[1]: %#v", r.buf[1])
+	}
+	if !r.buf[2].Equal(now3) {
+		t.Errorf("Unexpected r.buf[2]: %#v", r.buf[2])
+	}
+
+	if r.r != 0 {
+		t.Errorf("Unexpected r.r: %d", r.r)
+	}
+	if r.w != 2 {
+		t.Errorf("Unexpected r.w: %d", r.w)
+	}
+
+	r.put(now4)
+
+	if !r.buf[0].Equal(now4) {
+		t.Errorf("Unexpected r.buf[0]: %#v", r.buf[0])
+	}
+	if !r.buf[1].Equal(now2) {
+		t.Errorf("Unexpected r.buf[1]: %#v", r.buf[1])
+	}
+	if !r.buf[2].Equal(now3) {
+		t.Errorf("Unexpected r.buf[2]: %#v", r.buf[2])
+	}
+	if got := r.get(); !got.Equal(now2) {
+		// now2 is the oldest value in the buffer after r.put(now4)
+		t.Errorf("Unexpected r.get(): %#v", got)
+	}
+
+	if r.r != 1 {
+		t.Errorf("Unexpected r.r: %d", r.r)
+	}
+	if r.w != 0 {
 		t.Errorf("Unexpected r.w: %d", r.w)
 	}
 }
