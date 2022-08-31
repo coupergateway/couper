61181c54de9bd04551f3d90a934d8b225f731b3f
Marcel Ludwig <marcel.ludwig@avenga.com>
2022-08-31 08:53:47 +0200

    (docs) update searchDB indexing, styles (#566)
    * (docs) create searchdb index content from file for blocks without attrs
    
    * bump versions
    
    * refactor search result components and filtering
    
    * (docs) new style settings for app layout, search results

--- docs/website/layouts/default.vue
+++ docs/website/layouts/default.vue
@@ -1,52 +1,52 @@
 <script setup lang="ts">
-import Navbar from "~/components/Navbar.vue";
-import SideNavbar from "~/components/SideNavbar.vue";
+import Logo from "@/components/content/Logo";
+import GithubIcon from "@/components/content/GithubIcon";
+import SearchBar from "@/components/SearchBar";
+import SideNavbar from "@/components/SideNavbar";
 
 const { toc } = useContent();
 </script>
 
 <template>
-  <div class="antialiasing text-slate-500 bg-white">
-    <div class="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
-    </div>
-    <Navbar class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-slate-700/90" />
-    <div class="hidden lg:block fixed z-20 inset-0 pt-[7.56rem] left-0 right-auto w-[15rem] overflow-y-auto">
-      <SideNavbar class="lg:text-sm lg:leading-6
-      bg-slate-600 text-gray-400 pl-4 pt-2" />
-    </div>
-    <div class="max-w-8xl mx-auto sm:px-6">
-      <div class="lg:pl-[17rem]">
-          <main class="max-w-8xl pt-6 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16 relative z-100 mt-4
-            prose prose-slate prose-lg
-            prose-code:bg-sky-100 prose-code:text-sky-900 prose-code:p-1 prose-code:rounded-md prose-code:text-sm
-            prose-blockquote:bg-purple-100
-            prose-blockquote:rounded-md
-            prose-a:text-sky-600
-            hover:prose-a:text-amber-500
-          ">
-            <slot />
-          </main>
-        <footer class="text-sm leading-6 mt-12"><p>Copyright couper.io</p></footer>
+  <div class="antialiasing text-slate-700">
+    <header class="flex justify-between p-1 headerShadow">
+      <div class="relative w-2/5 flex items-center m-4">
+        <Logo class="box-border h-16 w-16 md:w-18 md:h-18 xl:w-22 xl:h-22" />
+        <h1 class="text-2xl font-medium align-top p-4">Couper Documentation</h1>
+        <div class="box-border text-base">
+          <div class="rounded-md bg-lime-500 p-1">edge</div>
+        </div>
       </div>
-        <nav v-if="toc && toc.links" class="fixed z-20 top-[7.56rem] bottom-0 right-0 w-[19.5rem] py-8 px-6 overflow-y-auto hidden xl:block
-        bg-slate-600 text-gray-400">
-            <h2 class="text-gray-100">On this Page</h2>
-            <ul v-if="toc && toc.links">
-              <li v-for="link in toc.links" :key="link.text">
-                <NuxtLink :href="`#${link.id}`">
-                  {{ link.text }}
+      <div class="flex justify-around w-full">
+        <SearchBar class="px-4 sm:px-8 lg:px-16 xl:px-5 mx-auto" />
+      </div>
+      <div class="w-1/5"><a href="https://github.com/avenga/couper" target="_blank"><github-icon class="w-8 h-8 mt-8" /></a></div>
+    </header>
+    <div class="flex justify-between m-2 p-2 min-h-screen">
+      <SideNavbar class="w-1/5 min-w-min grow-0 p-2" />
+      <main class="grow p-5 w-3/5
+        prose prose-slate prose-lg prose-code:bg-sky-100 prose-code:text-sky-900 prose-code:p-1 prose-code:rounded-md prose-code:text-sm prose-blockquote:bg-purple-100 prose-blockquote:rounded-md prose-a:text-sky-600 hover:prose-a:text-amber-500">
+        <slot />
+      </main>
+      <nav v-if="toc && toc.links" class="w-1/5 grow-0 p-2">
+        <h2 class="text-amber-500">On this Page</h2>
+        <ul v-if="toc && toc.links">
+          <li v-for="link in toc.links" :key="link.text">
+            <NuxtLink :href="`#${link.id}`">
+              {{ link.text }}
+            </NuxtLink>
+            <ul v-if="link.children">
+              <li v-for="child in link.children" :key="child.id">
+                <NuxtLink class="pl-2" :href="`#${child.id}`">
+                  {{ child.text }}
                 </NuxtLink>
-                <ul v-if="link.children">
-                  <li v-for="child in link.children" :key="child.id">
-                    <NuxtLink class="pl-2" :href="`#${child.id}`">
-                      {{ child.text }}
-                    </NuxtLink>
-                  </li>
-                </ul>
               </li>
             </ul>
-        </nav>
-      </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <footer class="col-span-3 text-sm leading-6 m-10"><p>Copyright couper.io</p></footer>
   </div>
 </template>
 
@@ -56,5 +56,9 @@ pre > code {
   background-color: inherit !important;
   padding: 0 !important;
   white-space: pre !important /* line breaks for copy/paste! */
+}
+
+.headerShadow {
+  box-shadow: rgb(203 203 203) 0 5px 10px 0;
 }
 </style>
