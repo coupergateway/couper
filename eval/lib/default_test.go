b38e6e2682b0fc84e1e1ae712d6b136c82967aca
Johannes Koch <johannes.koch@avenga.com>
2022-10-17 09:23:59 +0200

    added test for mixed types

---
+++ eval/lib/default_test.go
@@ -0,0 +1,52 @@
+package lib_test
+
+import (
+	"testing"
+
+	"github.com/zclconf/go-cty/cty"
+
+	"github.com/avenga/couper/config/configload"
+	"github.com/avenga/couper/config/request"
+	"github.com/avenga/couper/eval"
+	"github.com/avenga/couper/internal/test"
+)
+
+func TestDefaultErrors(t *testing.T) {
+	helper := test.New(t)
+
+	cf, err := configload.LoadBytes([]byte(`server {}`), "couper.hcl")
+	helper.Must(err)
+
+	tests := []struct {
+		name    string
+		args    []cty.Value
+		wantErr string
+	}{
+		{
+			"mixed types",
+			[]cty.Value{
+				cty.TupleVal([]cty.Value{
+					cty.StringVal("1"),
+				}),
+				cty.ObjectVal(map[string]cty.Value{
+					"a": cty.NumberIntVal(1),
+				}),
+			},
+			"all non-NilVal arguments must have the same type",
+		},
+	}
+
+	hclContext := cf.Context.Value(request.ContextType).(*eval.Context).HCLContext()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(subT *testing.T) {
+			_, err := hclContext.Functions["default"].Call(tt.args)
+			if err == nil {
+				subT.Error("Error expected")
+			}
+			if err != nil && err.Error() != tt.wantErr {
+				subT.Errorf("Wrong error message; expected %#v, got: %#v", tt.wantErr, err.Error())
+			}
+		})
+	}
+}
