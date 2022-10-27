eaa62dab9f996e93ac6bde6405702f99f76266b0
Johannes Koch <johannes.koch@avenga.com>
2022-10-27 10:48:26 +0200

    added function to read optional JSON mapping object from file or take provided map

--- config/reader/file.go
+++ config/reader/file.go
@@ -1,11 +1,33 @@
 package reader
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 
 	"github.com/avenga/couper/errors"
 )
+
+func ReadFromAttrFileJSONObjectOptional(context string, attributeValue map[string][]string, path string) (map[string][]string, error) {
+	readErr := errors.Configuration.Label(context + ": read error")
+	if attributeValue != nil && path != "" {
+		return nil, readErr.Message("configured attribute and file")
+	}
+
+	if path != "" {
+		fileContent, err := ReadFromFile(context, path)
+		if err != nil {
+			return nil, err
+		}
+		var m map[string][]string
+		if err = json.Unmarshal(fileContent, &m); err != nil {
+			return nil, readErr.Message("invalid file content")
+		}
+		return m, nil
+	}
+
+	return attributeValue, nil
+}
 
 func ReadFromAttrFile(context, attribute, path string) ([]byte, error) {
 	readErr := errors.Configuration.Label(context + ": read error")
--- config/reader/file_test.go
+++ config/reader/file_test.go
@@ -46,3 +46,43 @@ func TestReadFromAttrFile(t *testing.T) {
 		})
 	}
 }
+
+func TestReadFromAttrFileJSONObjectOptional(t *testing.T) {
+	expMap := map[string][]string{
+		"a": {"a1", "a2"},
+		"b": {"b1"},
+		"c": {},
+	}
+
+	type args struct {
+		context   string
+		attribute map[string][]string
+		path      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string][]string
+		wantErr bool
+	}{
+		{"not configured", args{context: "testcase"}, nil, false},
+		{"both configured", args{context: "testcase", attribute: expMap, path: "testdata/map.json"}, nil, true},
+		{"attr configured", args{context: "testcase", attribute: expMap, path: ""}, expMap, false},
+		{"path configured", args{context: "testcase", attribute: nil, path: "testdata/map.json"}, expMap, false},
+		{"path configured, invalid file content", args{context: "testcase", attribute: nil, path: "testdata/map_error.json"}, nil, true},
+		{"path configured, file not found", args{context: "testcase", attribute: nil, path: "testdata/file_not_found"}, nil, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(subT *testing.T) {
+			got, err := reader.ReadFromAttrFileJSONObjectOptional(tt.args.context, tt.args.attribute, tt.args.path)
+			if (err != nil) != tt.wantErr {
+				subT.Errorf("ReadFromAttrFileJSONObject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				subT.Errorf("ReadFromAttrFileJSONObject() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
