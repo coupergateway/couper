5a03b47b6b72d52835d44ffef126ebcb61a520b5
Johannes Koch <johannes.koch@avenga.com>
2022-11-04 12:40:53 +0100

    fix documentation

--- config/jwt_signing_profile.go
+++ config/jwt_signing_profile.go
@@ -4,7 +4,7 @@ import "github.com/hashicorp/hcl/v2"
 
 type JWTSigningProfile struct {
 	Claims             Claims         `hcl:"claims,optional" docs:"claims for the JWT payload, claim values are evaluated per request"`
-	Headers            hcl.Expression `hcl:"headers,optional" docs:"additional header fields for the JWT, {alg} and {typ} cannot be set"`
+	Headers            hcl.Expression `hcl:"headers,optional" docs:"additional header fields for the JWT, {typ} has the default value {JWT}, {alg} cannot be set"`
 	Key                string         `hcl:"key,optional" docs:"private key (in PEM format) for {RS*} and {ES*} variants or the secret for {HS*} algorithms. Mutually exclusive with {key_file}."`
 	KeyFile            string         `hcl:"key_file,optional" docs:"reference to file containing signing key. Mutually exclusive with {key}. See {key} for more information."`
 	Name               string         `hcl:"name,label,optional"`
