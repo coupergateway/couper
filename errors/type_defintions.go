19641d2affcbe37c3e2419589b9df1b8940c0c26
Johannes Koch <johannes.koch@avenga.com>
2023-01-19 10:25:12 +0100

    status code 401 for error type jwt

--- errors/type_defintions.go
+++ errors/type_defintions.go
@@ -10,7 +10,7 @@ var Definitions = []*Error{
 	AccessControl.Kind("basic_auth").Status(http.StatusUnauthorized),
 	AccessControl.Kind("basic_auth").Kind("basic_auth_credentials_missing").Status(http.StatusUnauthorized),
 
-	AccessControl.Kind("jwt"),
+	AccessControl.Kind("jwt").Status(http.StatusUnauthorized),
 	AccessControl.Kind("jwt").Kind("jwt_token_expired").Status(http.StatusUnauthorized),
 	AccessControl.Kind("jwt").Kind("jwt_token_invalid").Status(http.StatusUnauthorized),
 	AccessControl.Kind("jwt").Kind("jwt_token_missing").Status(http.StatusUnauthorized),
