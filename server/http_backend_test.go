90df2105af6eaf0adbd0f433e4ad4511f0ce8b64
Marcel Ludwig <1841067+malud@users.noreply.github.com>
2023-10-10 17:56:12 +0200

    Fix buffer condition and related body parse in combination with syncedVariables provided by each backend instance

--- server/http_backend_test.go
+++ server/http_backend_test.go
@@ -84,7 +84,7 @@ func TestBackend_MaxConnections_BodyClose(t *testing.T) {
 	helper := test.New(t)
 
 	origin := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second * 2) // always delay, ensures every req hit runs into max_conns issue
+		time.Sleep(time.Second) // always delay, ensures every req hit runs into max_conns issue
 
 		rw.Header().Set("Content-Type", "application/json")
 		b, err := json.Marshal(r.URL)
@@ -112,23 +112,31 @@ func TestBackend_MaxConnections_BodyClose(t *testing.T) {
 		"/ws",
 	}
 
-	for _, p := range paths {
-		deadline, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	t.Run("parallel", func(t *testing.T) {
+		for _, p := range paths {
+			p := p
+			t.Run("_"+p, func(st *testing.T) {
+				st.Parallel()
 
-		req, _ := http.NewRequest(http.MethodGet, "http://couper.dev:8080"+p, nil)
-		res, err := client.Do(req.WithContext(deadline))
-		cancel()
-		helper.Must(err)
+				h := test.New(st)
 
-		if res.StatusCode != http.StatusOK {
-			t.Errorf("want: 200, got %d", res.StatusCode)
+				req, _ := http.NewRequest(http.MethodGet, "http://couper.dev:8080"+p, nil)
+
+				deadline, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(len(paths)+1))
+				defer cancel()
+				res, err := client.Do(req.WithContext(deadline))
+				h.Must(err)
+
+				if res.StatusCode != http.StatusOK {
+					st.Errorf("want: 200, got %d", res.StatusCode)
+				}
+
+				_, err = io.Copy(io.Discard, res.Body)
+				h.Must(err)
+				h.Must(res.Body.Close())
+			})
 		}
-
-		_, err = io.Copy(io.Discard, res.Body)
-		helper.Must(err)
-
-		helper.Must(res.Body.Close())
-	}
+	})
 }
 
 // TestBackend_WithoutOrigin expects the listed errors to ensure no host from the client-request
