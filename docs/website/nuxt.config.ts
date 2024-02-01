63d69e23cc43e4ca219e404a9137cd6e5eed13fc
Marcel Ludwig <1841067+malud@users.noreply.github.com>
2024-02-01 15:51:02 +0100

    (ci) website action

--- docs/website/nuxt.config.ts
+++ docs/website/nuxt.config.ts
@@ -15,13 +15,12 @@ export default defineNuxtConfig({
         // baseURL: process.env.NODE_ENV === 'production' ? '/couper-docs/' : '/'
     },
     algolia: {
-        apiKey: '5551c3e4dfb61914988abf95fd9b762f',
+        apiKey: '5551c3e4dfb61914988abf95fd9b762f', // search only
         applicationId: 'MSIN2HU7WH',
         instantSearch: {
             theme: 'algolia'
         },
         crawler: {
-            apiKey: '<WriteKey>',
             indexName: 'docs',
             include: undefined, // all routes
             meta: ['title', 'description', '_path']
