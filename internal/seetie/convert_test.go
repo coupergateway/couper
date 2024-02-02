a861efbe4e089e4eb1d344f69b10cb40bd37601c
Johannes Koch <johannes.koch@milecrew.com>
2024-02-02 13:15:22 +0100

    HeaderToMapValue(): don't ignore number-only header field names

--- internal/seetie/convert_test.go
+++ internal/seetie/convert_test.go
@@ -94,3 +94,26 @@ func TestNumericKey(t *testing.T) {
 		t.Fatalf("entry for 01234 must be %q, but is %q", "foo", mappedValStr)
 	}
 }
+
+func TestNumericHeaderFieldName(t *testing.T) {
+	h := http.Header{
+		"01234": []string{"foo"},
+	}
+	val := HeaderToMapValue(h)
+	valType := val.Type()
+	if !valType.IsMapType() {
+		t.Fatalf("type must be map, but is %#v", valType)
+	}
+	values := val.AsValueMap()
+	if len(values) == 0 {
+		t.Fatal("map must not be empty")
+	}
+	mappedVal, ok := values["01234"]
+	if !ok {
+		t.Fatal("map lacks required key")
+	}
+	mappedValStr := mappedVal.AsString()
+	if "foo" != mappedVal.AsString() {
+		t.Fatalf("entry for 01234 must be %q, but is %q", "foo", mappedValStr)
+	}
+}
