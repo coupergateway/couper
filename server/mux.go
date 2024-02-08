3e5849ca2b25441fb97df59bcae2d8cea0d3b4fe
Johannes Koch <johannes.koch@milecrew.com>
2024-02-08 13:47:56 +0100

    for files base_path route: first add wildcard, then sort

--- server/mux.go
+++ server/mux.go
@@ -14,7 +14,6 @@ import (
 	"github.com/coupergateway/couper/errors"
 	"github.com/coupergateway/couper/handler"
 	"github.com/coupergateway/couper/handler/middleware"
-	"github.com/coupergateway/couper/utils"
 )
 
 // Mux is a http request router and dispatches requests
@@ -100,7 +99,7 @@ func (m *Mux) RegisterConfigured() {
 	}
 
 	for _, path := range sortedPathPatterns(m.opts.FileRoutes) {
-		mustAddRoute(m.fileRoot, utils.JoinOpenAPIPath(path, "/**"), m.opts.FileRoutes[path], false)
+		mustAddRoute(m.fileRoot, path, m.opts.FileRoutes[path], false)
 	}
 
 	for _, path := range sortedPathPatterns(m.opts.SPARoutes) {
