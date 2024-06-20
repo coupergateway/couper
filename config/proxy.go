4a1a3a75c5124c62228d6d50f8ecf61a0e42c560
Johannes Koch <johannes.koch@milecrew.com>
2024-06-20 22:19:23 +0200

    added unexpected_status attribute for proxy block

--- config/proxy.go
+++ config/proxy.go
@@ -46,10 +46,11 @@ func (p Proxy) Inline() interface{} {
 		meta.ResponseHeadersAttributes
 		meta.FormParamsAttributes
 		meta.QueryParamsAttributes
-		Backend        *Backend    `hcl:"backend,block" docs:"Configures a [backend](/configuration/block/backend) for the proxy request (zero or one). Mutually exclusive with {backend} attribute."`
-		ExpectedStatus []int       `hcl:"expected_status,optional" docs:"If defined, the response status code will be verified against this list of codes. If the status code not included in this list an {unexpected_status} error will be thrown which can be handled with an [{error_handler}](error_handler)."`
-		URL            string      `hcl:"url,optional" docs:"URL of the resource to request. May be relative to an origin specified in a referenced or nested {backend} block."`
-		Websockets     *Websockets `hcl:"websockets,block" docs:"Configures support for [websockets](/configuration/block/websockets) connections (zero or one). Mutually exclusive with {websockets} attribute."`
+		Backend          *Backend    `hcl:"backend,block" docs:"Configures a [backend](/configuration/block/backend) for the proxy request (zero or one). Mutually exclusive with {backend} attribute."`
+		ExpectedStatus   []int       `hcl:"expected_status,optional" docs:"If defined, the response status code will be verified against this list of codes. If the status code is not included in this list an {unexpected_status} error will be thrown which can be handled with an [{error_handler}](error_handler). Mutually exclusive with {unexpected_status}."`
+		UnexpectedStatus []int       `hcl:"unexpected_status,optional" docs:"If defined, the response status code will be verified against this list of codes. If the status code is included in this list an {unexpected_status} error will be thrown which can be handled with an [{error_handler}](error_handler). Mutually exclusive with {expected_status}."`
+		URL              string      `hcl:"url,optional" docs:"URL of the resource to request. May be relative to an origin specified in a referenced or nested {backend} block."`
+		Websockets       *Websockets `hcl:"websockets,block" docs:"Configures support for [websockets](/configuration/block/websockets) connections (zero or one). Mutually exclusive with {websockets} attribute."`
 	}
 
 	return &Inline{}
