78462a8628b2b28430235d90d952a3aae9c830c2
Marcel Ludwig <1841067+malud@users.noreply.github.com>
2024-07-08 08:53:42 +0200

    add fixed test; fix race cond

--- handler/ratelimit/limiter_test.go
+++ handler/ratelimit/limiter_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/coupergateway/couper/internal/test"
 )
 
-func TestNewLimiter_Sliding(t *testing.T) {
+func TestLimiter_Sliding(t *testing.T) {
 	helper := test.New(t)
 	logger, _ := test.NewLogger()
 
@@ -111,6 +111,7 @@ func TestNewLimiter_Sliding(t *testing.T) {
 		req, rerr := http.NewRequest(http.MethodGet, origin.URL, nil)
 		stHelper.Must(rerr)
 
+		var cancelReqMu sync.Mutex
 		var cancelReqFn func()
 		const cancelIdx = 2
 		var wg sync.WaitGroup
@@ -120,7 +121,9 @@ func TestNewLimiter_Sliding(t *testing.T) {
 				defer wg.Done()
 				rctx := ctx
 				if idx == cancelIdx {
+					cancelReqMu.Lock()
 					rctx, cancelReqFn = context.WithCancel(ctx)
+					cancelReqMu.Unlock()
 				}
 				_, e := limiter.RoundTrip(req.WithContext(rctx))
 				if e != nil && idx != cancelIdx {
@@ -140,7 +143,10 @@ func TestNewLimiter_Sliding(t *testing.T) {
 		// Cancel the request after some time so req-nr 3 is canceled while it should be in the queue.
 		d, _ := time.ParseDuration(period)
 		time.Sleep((cancelIdx + 1) * d)
+
+		cancelReqMu.Lock()
 		cancelReqFn()
+		cancelReqMu.Unlock()
 
 		wg.Wait()
 
@@ -150,7 +156,91 @@ func TestNewLimiter_Sliding(t *testing.T) {
 	})
 }
 
-func TestNewLimiter_Block(t *testing.T) {
+func TestLimiter_Fixed(t *testing.T) {
+	helper := test.New(t)
+	logger, _ := test.NewLogger()
+
+	ctx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
+
+	const period = "2s"
+
+	limits, err := ratelimit.ConfigureRateLimits(ctx, config.RateLimits{
+		&config.RateLimit{
+			Mode:         "wait",
+			Period:       period,
+			PerPeriod:    1,
+			PeriodWindow: "fixed",
+		},
+	}, logger.WithContext(ctx))
+	helper.Must(err)
+
+	backend := &http.Transport{
+		MaxConnsPerHost: 1,
+	}
+
+	limiter := ratelimit.NewLimiter(backend, nil)
+	if limiter != nil {
+		t.Errorf("expected nil Limiter, got %v", limiter)
+	}
+
+	limiter = ratelimit.NewLimiter(backend, limits)
+	if limiter == nil {
+		t.Errorf("expected configured Limiter, got %v", limiter)
+	}
+
+	t.Run("successful requests", func(st *testing.T) {
+		const maxRequests = 4
+		periodDuration, _ := time.ParseDuration(period)
+		var expectedDuration = periodDuration * maxRequests // first req resets the period too
+
+		stHelper := test.New(st)
+
+		reqCounter := &atomic.Int32{}
+		origin := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			reqCounter.Add(1)
+		}))
+		defer origin.Close()
+
+		req, rerr := http.NewRequest(http.MethodGet, origin.URL, nil)
+		stHelper.Must(rerr)
+
+		rctx, tCancel := context.WithCancel(ctx)
+		defer tCancel()
+
+		startTime := time.Now()
+		wg := sync.WaitGroup{}
+		wg.Add(maxRequests)
+		for i := 0; i < maxRequests; i++ {
+			go func() {
+				defer wg.Done()
+				resp, e := limiter.RoundTrip(req.WithContext(rctx))
+				if e != nil {
+					st.Errorf("unexpected error: %v", e)
+					return
+				}
+				if resp.StatusCode != http.StatusOK {
+					st.Errorf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+				}
+			}()
+			time.Sleep(periodDuration)
+		}
+		wg.Wait()
+		duration := time.Since(startTime)
+
+		if reqCounter.Load() != maxRequests {
+			st.Errorf("expected %d requests, got %d", maxRequests, reqCounter.Load())
+		}
+
+		if !fuzzyEqual(duration, expectedDuration, time.Millisecond*50) {
+			st.Errorf("expected duration around %v, got %v", expectedDuration, duration)
+		}
+		st.Logf("duration: %v, expected: %v", duration, expectedDuration)
+	})
+}
+
+func TestLimiter_Block(t *testing.T) {
 	helper := test.New(t)
 	logger, _ := test.NewLogger()
 
